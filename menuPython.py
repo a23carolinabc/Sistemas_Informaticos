8e903ac5be7ece9f4cd3474d1a20187df949ad44
Carolina <ladmin@debian>
2024-02-02 12:51:34 +0100

    Viernes 2/2

--- menuPython.py
+++ menuPython.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
-def prueba():
-    print("HOLA")
+def primo():
+    num = int (input("Introduce un entero"))
+    flag = 0
+    while flag >= 0:
+        for i in range (2, num, 1):
+            if (num%i == 0):
+                flag = 1
+                print ("No es primo")
+            if (flag == 0):
+                flag = -1
+                print ("Es primo")
+
 if __name__ == "__main__":
-    prueba()
