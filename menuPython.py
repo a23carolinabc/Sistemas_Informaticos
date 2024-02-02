f3f8e19657c7ac101742d3c711db8bca0edae919
Carolina <ladmin@debian>
2024-02-02 12:16:25 +0100

    Viernes 2/2

---
+++ menuPython.py
@@ -0,0 +1,5 @@
+"""SSH <> SH
+Operacións moi básicas pero sintácticamente correctas.
+Non tocache a parte de sistemas pura (percorrer directorios, permisos, información de ficheiros,...)
+Non se ve o resultado da execución das opcións, pensaches en meterlle un read dun caracter?"""
+import os
